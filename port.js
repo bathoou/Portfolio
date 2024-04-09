41c4329331592f1db1dadcccbfd6168adaca3385
Padmanban A <115460479+bathoou@users.noreply.github.com>
2024-04-09 23:50:25 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,15 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+
+  window.onload= function(){
+    document.getElementById("download").addEventListener("click",()=>{
+      return(
+        btn-container
+      )
+
+    })
+  } 
